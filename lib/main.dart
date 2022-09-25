e8e41fe5691fd941b31215f26bc841cc5eee4632
sankalp srivatava <Sankalp2015415@akgec.ac.in>
2022-09-25 16:31:39 +0530

    signup

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RuBaRu',
+      title: 'RuBaRu App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme:
